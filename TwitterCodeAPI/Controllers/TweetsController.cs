0c73094e5b5d6efe7e934d910816b5d39cf44795
jtprather <prather.justin@gmail.com>
2017-11-14 10:47:10 -0500

    Setup API to be Application-Only Auth using a generated bearer token, added tests around that. Updated LaunchSettings to just run a server on the TwitterCodeAPI launch.

--- TwitterCodeAPI/Controllers/TweetsController.cs
+++ TwitterCodeAPI/Controllers/TweetsController.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
 using Tweetinvi;
 using Tweetinvi.Models;
 using TwitterCodeAPI.Models;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace TwitterCodeAPI.Controllers
 {
@@ -10,18 +17,58 @@ namespace TwitterCodeAPI.Controllers
     [Route("api/[controller]")]
     public class TweetsController : Controller
     {
-        public TweetsController()
-        {
-        }
-
         const string CONSUMER_KEY = "Gui8jNqZ60AVlIV0UGTypJ7aq";
         const string CONSUMER_SECRET = "G0iUUYSYVYa4Y3PyyW7s915633tz4admTudfOTtGkoZwq4wjBp";
+        public string bearerToken { get; set; }
+
+        public TweetsController()
+        {
+            this.AppAuth();
+        }
+
+        private void AppAuth()
+        {
+            //get a BearerToken;
+            string bearerRequest = HttpUtility.UrlEncode(CONSUMER_KEY) + ":" + HttpUtility.UrlEncode(CONSUMER_SECRET);
+            //encode to base64String
+            bearerRequest = Convert.ToBase64String(Encoding.UTF8.GetBytes(bearerRequest));
+
+            //build web request
+            WebRequest request = WebRequest.Create("https://api.twitter.com/oauth2/token");
+            request.Headers.Add("Authorization", "Basic " + bearerRequest);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded;charset=UTF-8";
+
+            //stream web request to get auth token
+            string requestContent = "grant_type=client_credentials";
+            byte[] bytearrayRequestContent = Encoding.UTF8.GetBytes(requestContent);
+            System.IO.Stream requestStream = request.GetRequestStream();
+            requestStream.Write(bytearrayRequestContent, 0, bytearrayRequestContent.Length);
+            requestStream.Close();
+
+            //setup a jsonObject to get the respone token
+            string responseJson = string.Empty;
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                System.IO.Stream responseStream = response.GetResponseStream();
+                responseJson = new StreamReader(responseStream).ReadToEnd();
+            }
+
+            JObject jObject = JObject.Parse(responseJson);
+
+            //set the bearer token
+            bearerToken = jObject["access_token"].ToString();
+        }
+        
         
         // GET api/tweets
         [HttpGet]
         public IEnumerable<TimelineTweet> Get()
         {
-            Auth.SetUserCredentials(CONSUMER_KEY, CONSUMER_SECRET, "1273228404-0tdiu5Pwsgu2zt0xlfrlS9G38Q060UKEri2rupf", "iRPHq6jhq67KgFNtGLSZTo7u1ORx6r2KsqfYqts3mXJlH");
+            var appCreds = Auth.SetApplicationOnlyCredentials(CONSUMER_KEY, CONSUMER_SECRET, bearerToken);
+
             List<TimelineTweet> tweets = new List<TimelineTweet>();
 
             var userTimeline = Timeline.GetUserTimeline("salesforce",10);
--- TwitterCodeAPITests/TweetControllerTests.cs
+++ TwitterCodeAPITests/TweetControllerTests.cs
@@ -7,9 +7,15 @@ namespace TwitterCodeAPITests
 {
     public class TweetControllerTests
     {
+        [Fact]
+        public void TweetsController_AppAuth_GeneratesBearerToken()
+        {
+            var testController = new TweetsController();
+            Assert.NotNull(testController.bearerToken);
+        }
 
         [Fact]
-        public void TweetController_Get_ReturnsTweets()
+        public void TweetsController_Get_ReturnsTweets()
         {
             var testController = new TweetsController();
             var result = testController.Get();
